3f41324fc00c7b94afd16dd5741298d675ec1e36
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-09-07 17:59:41 +0800

    Modify package.json and db configs.

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,7 @@ require("dotenv/config");
 const { UNAME, PASS, DB_NAME, ENV } = process.env;
 
 const DEV_MONGO_URI = `mongodb://127.0.0.1/${DB_NAME}`;
-const PROD_MONGO_URI = `mongodb://admin:admin@ec2-18-141-205-53.ap-southeast-1.compute.amazonaws.com/${DB_NAME}`;
-
+const PROD_MONGO_URI = `mongodb+srv://${UNAME}:${PASS}@ecluster0.chup1pq.mongodb.net/${DB_NAME}`;
 const MONGO_URI = ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
 const connection = mongoose
   .connect(MONGO_URI)
