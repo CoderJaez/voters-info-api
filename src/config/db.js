4e641e3c60bd7aaa52ccc66049e1d6bfe4d66cef
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-10-01 09:24:57 +0800

    Fix again

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,8 @@ require("dotenv/config");
 const { UNAME, PASS, DB_NAME, ENV } = process.env;
 
 const DEV_MONGO_URI = `mongodb://127.0.0.1/${DB_NAME}`;
-const PROD_MONGO_URI = `mongodb+srv://root:chummy123@cluster0.sh1oc.mongodb.net/${DB_NAME}`;
-// const MONGO_URI = ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
-const MONGO_URI = PROD_MONGO_URI;
+const PROD_MONGO_URI = `mongodb+srv://${UNAME}:${PASS}@cluster0.sh1oc.mongodb.net/${DB_NAME}`;
+const MONGO_URI = ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
 const connection = mongoose
   .connect(MONGO_URI)
   .then(() => {
